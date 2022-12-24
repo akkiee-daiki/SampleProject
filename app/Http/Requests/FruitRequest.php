7d50b8fc74a9aaf2a4489c7ccbaf538b1315af4c
akkiee-daiki <daikiakibayashi@aol.com>
2022-12-24 11:54:13 +0900

    revise "{" positon

--- app/Http/Requests/FruitRequest.php
+++ app/Http/Requests/FruitRequest.php
@@ -4,8 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class FruitRequest extends FormRequest
-{
+class FruitRequest extends FormRequest {
     /**
      * @return array<string, mixed>
      */
